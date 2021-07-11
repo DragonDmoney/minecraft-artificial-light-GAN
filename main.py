6177833ecac6d36aa42f0e0bbdd07096cccf41bc
captainadd49 <captainadd49@gmail.com>
2021-07-11 13:55:40 -0400

    added progress bar, and test data

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import cv2
 import sys
-v = cv2.VideoCapture(r'C:\Users\addis\Videos\minecraft-shaders-gan\vanilla.mp4')
-s = cv2.VideoCapture(r'C:\Users\addis\Videos\minecraft-shaders-gan\shader.mp4')
+from tqdm import tqdm
+v = cv2.VideoCapture(r'C:\Users\addis\Videos\minecraft-shaders-gan\test default.mp4')
+s = cv2.VideoCapture(r'C:\Users\addis\Videos\minecraft-shaders-gan\shader test.mp4')
 
-from tqdm import tdqm LENGTH = 10 # Number of iterations required to fill
+length = 400
 
-pbar pbar = tqdm(total=LENGTH)
+pbar = tqdm(total=length)
 pbar.update(n=1)
 
-length = 21600
 
 count = 1
 for i in range(4240000):
@@ -17,9 +17,9 @@ for i in range(4240000):
     if i % 5 == 0:
         iv =cv2.resize(iv,[192,108])
         iis =cv2.resize(iis,[192,108])
-        cv2.imwrite('data\shader\\'+str(count)+'.jpg',iv)
-        cv2.imwrite('data\default\\'+str(count)+'.jpg',iis)
+        cv2.imwrite('data\shader test\\'+str(count)+'.jpg',iis)
+        cv2.imwrite('data\default test\\'+str(count)+'.jpg', iv)
         sys.stdout.flush()
-        sys.stdout.write('\rCount: '+str(count)+' i: ' + str(i))
+        # sys.stdout.write('\rCount: '+str(count)+' i: ' + str(i))
         count +=1
         pbar.update(n=1)
